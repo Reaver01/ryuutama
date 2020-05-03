d95136d327701dbc738f127d5aada0db199de873
Reaver01 <jawmac@gmail.com>
2020-05-03 02:06:25 -0600

    Started Sheet

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -54,8 +54,56 @@ export class RyuutamaActorSheet extends ActorSheet {
             li.slideUp(200, () => this.render(false));
         });
 
-        // Add or Remove Attribute
-        html.find(".attributes").on("click", ".attribute-control", this._onClickAttributeControl.bind(this));
+        // Check Buttons
+        html.find('.check-button').click(ev => {
+            if (ev.target.value === "Travel") {
+                const actor = this.actor;
+                const formula = `1d${actor.data.data.attributes.str.value} + 1d${actor.data.data.attributes.dex.value}`;
+                const r = new Roll(formula);
+                const roll = r.roll();
+                roll.toMessage({
+                    flavor: `${actor.name} rolled a Travel Check`
+                });
+            } else if (ev.target.value === "Direction") {
+                const actor = this.actor;
+                const formula = `1d${actor.data.data.attributes.int.value} + 1d${actor.data.data.attributes.int.value}`;
+                const r = new Roll(formula);
+                const roll = r.roll();
+                roll.toMessage({
+                    flavor: `${actor.name} rolled a Direction Check`
+                });
+            } else if (ev.target.value === "Camp") {
+                const actor = this.actor;
+                const formula = `1d${actor.data.data.attributes.dex.value} + 1d${actor.data.data.attributes.int.value}`;
+                const r = new Roll(formula);
+                const roll = r.roll();
+                roll.toMessage({
+                    flavor: `${actor.name} rolled a Camp Check`
+                });
+            } else if (ev.target.title === "Condition") {
+                const actor = this.actor;
+                const formula = `1d${actor.data.data.attributes.str.value} + 1d${actor.data.data.attributes.spi.value}`;
+                const r = new Roll(formula);
+                const roll = r.roll();
+                roll.toMessage({
+                    flavor: `${actor.name} rolled a Condition Check`
+                });
+                actor.update({
+                    "data.attributes.condition.value": roll._total
+                })
+            } else if (ev.target.title === "Initiative") {
+                const actor = this.actor;
+                const formula = `1d${actor.data.data.attributes.dex.value} + 1d${actor.data.data.attributes.int.value}`;
+                const r = new Roll(formula);
+                const roll = r.roll();
+                roll.toMessage({
+                    flavor: `${actor.name} rolled for Initiative`
+                });
+                actor.update({
+                    "data.attributes.initiative.value": roll._total
+                })
+            }
+        });
     }
 
     /* -------------------------------------------- */
@@ -71,66 +119,13 @@ export class RyuutamaActorSheet extends ActorSheet {
 
     /* -------------------------------------------- */
 
-    /**
-     * Listen for click events on an attribute control to modify the composition of attributes in the sheet
-     * @param {MouseEvent} event    The originating left click event
-     * @private
-     */
-    async _onClickAttributeControl(event) {
-        event.preventDefault();
-        const a = event.currentTarget;
-        const action = a.dataset.action;
-        const attrs = this.object.data.data.attributes;
-        const form = this.form;
-
-        // Add new attribute
-        if (action === "create") {
-            const nk = Object.keys(attrs).length + 1;
-            let newKey = document.createElement("div");
-            newKey.innerHTML = `<input type="text" name="data.attributes.attr${nk}.key" value="attr${nk}"/>`;
-            newKey = newKey.children[0];
-            form.appendChild(newKey);
-            await this._onSubmit(event);
-        }
-
-        // Remove existing attribute
-        else if (action === "delete") {
-            const li = a.closest(".attribute");
-            li.parentElement.removeChild(li);
-            await this._onSubmit(event);
-        }
-    }
-
-    /* -------------------------------------------- */
-
     /** @override */
     _updateObject(event, formData) {
-
-        // Handle the free-form attributes list
-        const formAttrs = expandObject(formData).data.attributes || {};
-        const attributes = Object.values(formAttrs).reduce((obj, v) => {
-            let k = v["key"].trim();
-            if (/[\s\.]/.test(k)) return ui.notifications.error("Attribute keys may not contain spaces or periods");
-            delete v["key"];
-            obj[k] = v;
-            return obj;
-        }, {});
-
-        // Remove attributes which are no longer used
-        for (let k of Object.keys(this.object.data.data.attributes)) {
-            if (!attributes.hasOwnProperty(k)) attributes[`-=${k}`] = null;
-        }
-
-        // Re-combine formData
-        formData = Object.entries(formData).filter(e => !e[0].startsWith("data.attributes")).reduce((obj, e) => {
-            obj[e[0]] = e[1];
-            return obj;
-        }, {
-            _id: this.object._id,
-            "data.attributes": attributes
-        });
-
         // Update the Actor
+        formData["data.health.max"] = formData["data.attributes.str.value"] * 2;
+        formData["data.mental.max"] = formData["data.attributes.spi.value"] * 2;
+        const levels = [0, 100, 600, 1200, 2000, 3000, 4200, 5800, 7500, 10000, 100000000];
+        formData["data.attributes.level.value"] = levels.findIndex(i => i > Number(formData["data.attributes.exp.value"]));
         return this.object.update(formData);
     }
 }
