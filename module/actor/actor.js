a3bcfffd82e8ef5b2c5216411fd5a31d6095dc36
Reaver01 <jawmac@gmail.com>
2020-05-06 23:32:21 -0600

    terrain, weather, effects, checks

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -29,22 +29,41 @@ export class RyuutamaActor extends Actor {
         const data = actorData.data;
         const items = actorData.items;
 
-        // Health Points
-        const oldHP = data.hp.max;
-        data.hp.max = data.attributes.str.value * 2;
-        if (oldHP != data.hp.max) {
-            data.hp.value = data.hp.max;
-        }
-
-        // Mental Points
-        const oldMP = data.mp.max;
-        data.mp.max = data.attributes.spi.value * 2;
-        if (oldMP != data.mp.max) {
-            data.mp.value = data.mp.max;
-        }
-
         // Level
         data.attributes.level.value = RYUU.CHARACTER_EXP_LEVELS.findIndex(i => i > Number(data.attributes.exp.value));
+        data.attributes.statIncreases.earned = (data.attributes.level.value - 1) * 3;
+        if (data.attributes.statIncreases.hp + data.attributes.statIncreases.mp > data.attributes.statIncreases.earned) {
+            data.attributes.statIncreases.hp = data.attributes.statIncreases.mp = 0;
+        }
+        data.attributes.statIncreases.hp = data.attributes.statIncreases.hp < 0 ? 0 : data.attributes.statIncreases.hp;
+        data.attributes.statIncreases.mp = data.attributes.statIncreases.mp < 0 ? 0 : data.attributes.statIncreases.mp;
+
+        // Health Points
+        data.hp.max = data.attributes.str.value * 2;
+        const hpItems = items.filter(i => i.data.enchantments.find(e => e.data.hpMod !== 0) !== undefined && i.data.equipped);
+        hpItems.forEach(item => {
+            let hpEnchantments = item.data.enchantments.filter(e => e.data.hpMod !== 0);
+            hpEnchantments.forEach(enchantment => {
+                data.hp.max += enchantment.data.hpMod;
+            });
+        });
+        data.hp.max += data.attributes.statIncreases.hp;
+
+        // Mental Points
+        data.mp.max = data.attributes.spi.value * 2;
+        const mpItems = items.filter(i => i.data.enchantments.find(e => e.data.mpMod !== 0) !== undefined && i.data.equipped);
+        mpItems.forEach(item => {
+            let mpEnchantments = item.data.enchantments.filter(e => e.data.mpMod !== 0);
+            mpEnchantments.forEach(enchantment => {
+                data.mp.max += enchantment.data.mpMod;
+            });
+        });
+        data.mp.max += data.attributes.statIncreases.mp;
+
+        // Don't allow values under min or over max
+        data.hp.value = Math.clamped(data.hp.value, 0, data.hp.max);
+        data.mp.value = Math.clamped(data.mp.value, 0, data.mp.max);
+        data.attributes.condition.value = Math.clamped(data.attributes.condition.value, 0, data.attributes.condition.max);
 
         // Carrying capacity
         let str = Number(data.attributes.str.value);
@@ -52,13 +71,16 @@ export class RyuutamaActor extends Actor {
             str = RYUU.DICE[RYUU.DICE.findIndex(i => i === str) + 1];
         }
 
-        data.attributes.capacity.max = Number(str) + 3;
+        data.attributes.capacity.max = Number(str) + 2 + data.attributes.level.value;
         const carried = items.filter(i => i.data.equipped === false && i.data.size !== undefined);
         const equipped = items.filter(i => i.data.equipped === true && i.data.size !== undefined);
 
         let carriedWeight = 0;
         carried.forEach(item => {
-            carriedWeight += Number(item.data.size);
+            let weightless = item.data.enchantments.find(e => e.data.weightless)
+            if (weightless === undefined) {
+                carriedWeight += Number(item.data.size);
+            }
         });
         data.attributes.capacity.value = carriedWeight;
 
@@ -68,6 +90,23 @@ export class RyuutamaActor extends Actor {
         });
         data.attributes.capacity.equipped = equippedWeight;
 
+        // Terrain
+        for (const name in data.traveling) {
+            if (data.traveling.hasOwnProperty(name)) {
+                data.traveling[name] = 0;
+                let mod = items.filter(i => i.data[name] && i.data.equipped);
+                mod.forEach(item => {
+                    data.traveling[name] += item.data.itemBonus;
+                });
+            }
+        }
+
+        // Effects
+        for (const name in data.effects) {
+            if (data.effects.hasOwnProperty(name) && data.immunity[name]) {
+                data.effects[name] = 0;
+            }
+        }
     }
 
 
